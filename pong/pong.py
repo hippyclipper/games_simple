594700496e65f693451fa7250c4016070c3fbdc0
hippyclipper <cameron_moon321@hotmail.com>
2022-05-09 17:50:13 -0700

    escape key to quit

--- pong/pong.py
+++ pong/pong.py
@@ -284,8 +284,9 @@ game = Game()
 while not done:
     
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             done = True
+            
             
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:           
             pass
