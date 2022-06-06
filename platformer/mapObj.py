9ba2af893bb3a843db2ef16ba33eb84bb0165af2
hippyclipper <cameron_moon321@hotmail.com>
2022-06-05 22:10:20 -0700

    height and width of map

--- platformer/mapObj.py
+++ platformer/mapObj.py
@@ -6,3 +6,15 @@ class Map(GameObject):
         super().__init__(0,0)
         self.filePath = "./map.txt"
         self.mapKey = {"wall": "#", "air": ".", "player": "$", "end": "@"}
+        self.widthNum = 0
+        self.heightNum = 0
+        file = open(self.filePath, "r")
+        for x in file:
+            self.heightNum += 1
+            self.widthNum = max(self.widthNum, len(x))
+        file.close()
+        
+        
+                                 
+            
+        
