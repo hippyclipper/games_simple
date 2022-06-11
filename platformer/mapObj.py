ade454d5f34997e3ef46a8c6af743cebca3f1b16
hippyclipper <cameron_moon321@hotmail.com>
2022-06-11 13:39:52 -0700

    list of tiles now

--- platformer/mapObj.py
+++ platformer/mapObj.py
@@ -20,20 +20,32 @@ class Map(GameObject):
         self.mapKey = {"wall": "#", "air": ".", "player": "$", "end": "@"}
         self.widthNum = 0
         self.heightNum = 0
-        self.level = [Tile(height//2, width//2)]
+        self.level = []
         file = open(self.filePath, "r")
+        
         for x in file:
             self.heightNum += 1
-            self.widthNum = max(self.widthNum, len(x))          
+            self.widthNum = max(self.widthNum, len(x))
+        self.tileW = width/self.widthNum
+        self.tileH = height/self.heightNum
+        file.close()
+        file = open(self.filePath, "r")
+        for x,line in enumerate(file):
+            self.level.append([])
+            for y,tile in enumerate(line):
+                self.level[x].append(Tile(x,y))
+                
         file.close()
         
     def update(self):
-        for tile in self.level:
-            tile.update()
+        for row in self.level:
+            for tile in row:
+                tile.update()
         
     def draw(self):
-        for tile in self.level:
-            tile.draw()
+        for row in self.level:
+            for tile in row:
+                tile.draw()
         
         
                                  
